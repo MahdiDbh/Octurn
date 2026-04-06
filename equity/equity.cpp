72bac1119bbf438aa919ae9b73f5d2df76ed07f5
daniilvedishchev <daniil.vedishchev@gmail.com>
2026-04-06 21:37:23 +0200

    upd: equity structure

--- equity/equity.cpp
+++ equity/equity.cpp
@@ -1,12 +1,19 @@
 #include "equity.hpp"
 
-Equity::Equity(std::vector<double>&& open,
-    std::vector<double>&& high,std::vector<double>&& low,
-    std::vector<double>&& close,std::vector<double>&& volume, std::vector<uint64_t>&& timestamp){
-    assetData_["open"] = std::move(open);
-    assetData_["high"] = std::move(high);
-    assetData_["low"] = std::move(low);
-    assetData_["close"] = std::move(close);
-    assetData_["volume"] = std::move(volume);
-    assetData_["timestamp"] = std::move(timestamp);
+Equity::Equity(std::vector<double>&& open,std::vector<double>&& high,std::vector<double>&& low,std::vector<double>&& close,std::vector<double>&& volume, std::vector<uint64_t>&& timestamp){
+    symbol_.open = std::move(open);
+    symbol_.high = std::move(high);
+    symbol_.low = std::move(low);
+    symbol_.close = std::move(close);
+    symbol_.volume = std::move(volume);
+    symbol_.timestamp = std::move(timestamp);
+}
+
+void Equity::constructTimestampIdxMap(){
+    size_t idx{0};
+    timestampIndexMap_.reserve(symbol_.timestamp.size());
+    for (const uint64_t& timestamp : symbol_.timestamp){
+        timestampIndexMap_[timestamp] = idx;
+        idx++;
+    }
 }
--- equity/equity.hpp
+++ equity/equity.hpp
@@ -1,11 +1,19 @@
 #include <vector>
 #include <unordered_map>
 
+struct EquityData {
+    std::vector<double> open, high, low, close, volume;
+    std::vector<uint64_t> timestamp;
+};
+
 struct Equity {
-    std::unordered_map<uint64_t,size_t> indexMap_;
-    std::unordered_map<std::string,std::variant<std::vector<uint64_t>,std::vector<double>>> assetData_;
+    std::unordered_map<uint64_t,size_t> timestampIndexMap_;
+    EquityData symbol_;
+    
 
     Equity(std::vector<double>&& open,std::vector<double>&& high,
         std::vector<double>&& low,std::vector<double>&& close,
         std::vector<double>&& volume,std::vector<uint64_t>&& timestamp);
+
+    void constructTimestampIdxMap();
 };
